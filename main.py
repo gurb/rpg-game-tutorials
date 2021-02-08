7578ccd0b6759152e138d7474fd283a5cf1f1857
Gürbüz Uğurgül <herbokvar@outlook.com>
2021-02-08 22:21:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 import random
 
+from player import Player
+
 # we initialize pygame module
 pygame.init()
 
 # create a surface represent our window
 screen = pygame.display.set_mode((640, 480))
+
+sprites_group = pygame.sprite.Group()
+
+player = Player(sprites_group, screen.get_rect().center, (25,25), (0,0,255))
 
 def main():
     running = True
@@ -16,7 +22,7 @@ def main():
                 running = False
         # draw
         screen.fill((255,255,255))
-        pygame.draw.circle(screen, (255,0,0), (random.randint(0, 640), 240), 20)
+        sprites_group.draw(screen)
         
         # update
         pygame.display.flip()
