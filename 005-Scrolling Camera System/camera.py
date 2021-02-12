a38a71996d72a15c6bdcc1af7d5505d21f2344d8
Gürbüz Uğurgül <herbokvar@outlook.com>
2021-02-12 23:35:48 +0300

    Create camera.py

---
+++ 005-Scrolling Camera System/camera.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.math import Vector2
+
+class Camera:
+    def __init__(self, target, width, height):
+        self.width = width
+        self.height = height
+        self.camera = Vector2(target.pos.x, target.pos.y)
+        self.target = target
+        self.offset = Vector2(0,0)
+
+    def scroll(self):
+        self.pointTarget = self.target.pos - self.camera
+        self.camera += self.pointTarget * 1
+        print(self.camera)
+        self.offset = -self.camera + Vector2(self.width/2, self.height/2)
